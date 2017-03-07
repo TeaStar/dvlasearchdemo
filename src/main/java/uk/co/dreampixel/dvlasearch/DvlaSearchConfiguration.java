050d66cf3aa618b579d13a13ee14b2738541a75a
Dan Hunter <danhu@motabilityoperations.co.uk>
2017-03-07 16:01:59 +0000

    Added proxy settings

--- src/main/java/uk/co/dreampixel/dvlasearch/DvlaSearchConfiguration.java
+++ src/main/java/uk/co/dreampixel/dvlasearch/DvlaSearchConfiguration.java
@@ -1,14 +1,57 @@
 package uk.co.dreampixel.dvlasearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.ProxyAuthenticationStrategy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 public class DvlaSearchConfiguration {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @Value("${dvla.search.proxy.host}")
+    private String proxyServer;
+
+    @Value("${dvla.search.proxy.port}")
+    private int proxyPort;
+
+    @Value("${dvla.search.proxy.user}")
+    private String proxyUser;
+
+    @Value("${dvla.search.proxy.pass}")
+    private String proxyPass;
+
+    @Bean public RestTemplate restTemplate() {
+
+        CredentialsProvider credentialProvider = new BasicCredentialsProvider();
+        credentialProvider.setCredentials(new AuthScope(proxyServer, proxyPort),
+                                          new UsernamePasswordCredentials(proxyUser, proxyPass));
+
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        httpClientBuilder.useSystemProperties();
+        httpClientBuilder.setProxy(new HttpHost(proxyServer, proxyPort));
+        httpClientBuilder.setDefaultCredentialsProvider(credentialProvider);
+        httpClientBuilder.setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
+
+        CloseableHttpClient client = httpClientBuilder.build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(client);
+
+        return new RestTemplate(requestFactory);
     }
+
 }
--- src/main/java/uk/co/dreampixel/dvlasearch/client/DvlaSearch.java
+++ src/main/java/uk/co/dreampixel/dvlasearch/client/DvlaSearch.java
@@ -50,4 +50,6 @@ public class DvlaSearch {
                                    .encode()
                                    .toUri();
     }
+
+
 }
